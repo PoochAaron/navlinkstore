4cebea19dd0b0f63af26381ca73e21ce6c783896
PoochAaron <szenczi.viktor@gmail.com>
2022-03-05 18:00:14 +0100

    webpage

---
+++ Files/webmcc_eles.bat
@@ -0,0 +1,4 @@
+@echo off
+@echo ~~~ WebMCC Inditasa ~~~
+@echo /// Kerem varjon \\\
+%systemroot%\system32\windowspowershell\v1.0\powershell.exe -executionpolicy bypass -file "\\intranet.nav.gov.hu\NETLOGON\WEBMCC\webmcc_eles.ps1"
